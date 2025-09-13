66e31c58252e75924fb2c6c7f39685439e77c1fe
the0hdDev <theowimber@gmail.com>
2025-09-13 14:21:22 +0000

    add comment functionallity to lexer

--- lexer/lexer_main.cpp
+++ lexer/lexer_main.cpp
@@ -1,17 +1,61 @@
 #include "lexer_main.h"
 
-char lex::Lexer::gettok() {
+using namespace lex;
+
+char Lexer::gettok() {
     static char LastChar = ' ';
     while (isspace(LastChar)) {
         LastChar = getchar();
     }
-    if (isalpha(LastChar)) {
-        while (isalnum((LastChar + getchar()))) {
-            lex::Lexer::IdentStr += LastChar;
-            switch (lex::Lexer::token) {
-                case 
-            };
+    if (isalpha(LastChar))
+    {
+        // identifier: [a-zA-Z][a-zA-Z0-9]*
+        Lexer::IdentStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            Lexer::IdentStr += LastChar;
+
+        if (Lexer::IdentStr == "func")
+        {
+            return Lexer::token_func;
         }
 
+        if (Lexer::IdentStr == "extern")
+        {
+            return Lexer::token_extern;
+        }
+
+        if (Lexer::IdentStr == "return"){
+            return Lexer::token_return;
+        }
     }
+
+
+    if (isdigit(LastChar) || LastChar == '.')
+    {
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+        NumVal = strtod(NumStr.c_str(), 0);
+        return token_number;
+    }
+
+    if (LastChar == '/') {
+        int NextChar = getchar();
+        if (NextChar == '/') {
+            do {
+                LastChar = getchar();
+            } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+            if (LastChar != EOF)
+                return gettok();
+        } else {
+            LastChar = NextChar;
+            return token_slash;
+        }
+    }
+
+
+    return Lexer::token_identifier;
 }
--- lexer/lexer_main.h
+++ lexer/lexer_main.h
@@ -5,20 +5,28 @@ namespace lex
 {
     class Lexer {
         public: 
-        enum token {
-            token_eof = -1,
-            // commands
+        enum Token {
+            tok_eof = -1,
+
+            // keywords
             token_func = -2,
-            token_extern = -3, 
+            token_extern = -3,
+
             // primary
-            token_indentifier = -4,
+            token_identifier = -4,
+            token_number = -5,
+
+            // operators
+            token_plus = -6,
+            token_minus = -7,
+            token_star = -8,
+            token_slash = -9,
         };
+
         static char gettok(); 
         private:
         static std::string IdentStr;
         static double NumVal;
 
-
-
-    }
+    };
 }
