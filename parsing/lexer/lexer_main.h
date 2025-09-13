6351612e72d3025ac2c02713fb2d53ef846cc191
the0hdDev <theowimber@gmail.com>
2025-09-13 14:53:37 +0000

    restructure

--- parsing/lexer/lexer_main.h
+++ parsing/lexer/lexer_main.h
@@ -11,16 +11,18 @@ namespace lex
             // keywords
             token_func = -2,
             token_extern = -3,
+            token_return = - 4,
 
             // primary
-            token_identifier = -4,
-            token_number = -5,
+            token_identifier = -5,
+            token_number = -6,
+
 
             // operators
-            token_plus = -6,
-            token_minus = -7,
-            token_star = -8,
-            token_slash = -9,
+            token_plus = -7,
+            token_minus = -8,
+            token_star = -9,
+            token_slash = -10,
         };
 
         static char gettok(); 
